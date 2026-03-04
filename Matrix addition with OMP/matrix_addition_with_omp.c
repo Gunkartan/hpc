5220b5345ac7780b780c70ca47819db4e6d33315
Karun Tancharoen <harukajidoto@gmail.com>
2026-03-04 23:04:50 +0700

    chore(files): stop tracking text files again

--- Matrix addition with OMP/matrix_addition_with_omp.c
+++ Matrix addition with OMP/matrix_addition_with_omp.c
@@ -3,27 +3,42 @@
 #include <omp.h>
 
 int main() {
-    FILE *fa = fopen("small_matrix_a.txt", "r");
-    FILE *fb = fopen("small_matrix_b.txt", "r");
-    FILE *fc = fopen("small_result.txt", "w");
+    FILE *fa = fopen("large_matrix_a.txt", "r");
+    FILE *fb = fopen("large_matrix_b.txt", "r");
+    FILE *fc = fopen("large_result.txt", "w");
+
+    if (fa == NULL || fb == NULL || fc == NULL) {
+        printf("Error opening the files\n");
+
+        return 1;
+    }
+
     int rows, cols;
     fscanf(fa, "%d %d", &rows, &cols);
     fscanf(fb, "%d %d", &rows, &cols);
-    int a[rows][cols];
-    int b[rows][cols];
-    int c[rows][cols];
+    double **a = malloc(rows * sizeof(double *));
+    double **b = malloc(rows * sizeof(double *));
+    double **c = malloc(rows * sizeof(double *));
+
+    for (int i = 0; i < rows; i++) {
+        a[i] = malloc(cols * sizeof(double));
+        b[i] = malloc(cols * sizeof(double));
+        c[i] = malloc(cols * sizeof(double));
+    }
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            fscanf(fa, "%d", &a[i][j]);
+            fscanf(fa, "%lf", &a[i][j]);
         }
     }
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            fscanf(fb, "%d", &b[i][j]);
+            fscanf(fb, "%lf", &b[i][j]);
         }
     }
+
+    double start = omp_get_wtime();
 
     #pragma omp parallel for collapse(2)
 
@@ -33,11 +48,13 @@ int main() {
         }
     }
 
+    double end = omp_get_wtime();
+    printf("The execution time is %f seconds\n", end - start);
     fprintf(fc, "%d %d\n", rows, cols);
 
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            fprintf(fc, "%d", c[i][j]);
+            fprintf(fc, "%.3f ", c[i][j]);
         }
 
         fprintf(fc, "\n");
@@ -46,6 +63,16 @@ int main() {
     fclose(fa);
     fclose(fb);
     fclose(fc);
+
+    for (int i = 0; i < rows; i++) {
+        free(a[i]);
+        free(b[i]);
+        free(c[i]);
+    }
+
+    free(a);
+    free(b);
+    free(c);
 
     return 0;
 }
