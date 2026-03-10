212e4548b5ddb30e67a49b7b9c986eb58253f5a6
Karun Tancharoen <harukajidoto@gmail.com>
2026-03-10 18:00:29 +0700

    feat(code): add the N-queens sequential code

---
+++ N-queens problem/n_queens_sequential.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int is_safe(int board[], int col, int row) {
+    for (int i = 0; i < col; i++) {
+        if (board[i] == row) {
+            return 0;
+        }
+
+        if (abs(col - i) == abs(row - board[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+void solve_n_queens(int board[], int col, int n, long long *solutions) {
+    if (col == n) {
+        (*solutions)++;
+
+        return;
+    }
+
+    for (int row = 0; row < n; row++) {
+        if (is_safe(board, col, row)) {
+            board[col] = row;
+            solve_n_queens(board, col + 1, n, solutions);
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    int *board = malloc(n * sizeof(int));
+
+    if (board == NULL) {
+        printf("Memory allocation failed\n");
+
+        return 1;
+    }
+
+    long long solutions = 0;
+    solve_n_queens(board, 0, n, &solutions);
+    printf("The total number of solutions with N being %d is %lld\n", n, solutions);
+    free(board);
+
+    return 0;
+}
